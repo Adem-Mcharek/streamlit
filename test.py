61f2ef38c1bb78e3f896448c9b632780c36029d6
Adem-Mcharek <122184635+Adem-Mcharek@users.noreply.github.com>
2024-04-10 22:14:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,3 @@
-"""
-    This file demonstrates how to use the OpenAI Assistants API with Streamlit.
-    Users upload a CSV and the assistant writes an article about the data. For
-    this to work, you'll need to:
-
-    1. Install the streamlit and openai packages
-    2. Get an OpenAI API key
-    3. Set an environment variable called OPENAI_API_KEY with your API key
-    4. Create an assistant with Code Interpreter on, and this system message:
-    "You are an experienced data journalist. You receive a CSV of data from a
-    user. You write code to find interesting patterns in the data. You choose
-    the most interesting of these patterns and write a 250-word article about
-    them. You write the headline for the article and then the article itself.
-    You do not ask for feedback from the user at any point. You independently
-    look for trends, independently write the article, and then provide the
-    article to the user to review."
-
-    Once you've done that, replace the <assistant_id> in the code below with 
-    your own assistant ID. Then run the code with streamlit run <filename.py>.
-
-"""
-
-
-
-
 import openai
 import streamlit as st
 import os
@@ -31,9 +6,7 @@ import openai
 from dotenv import load_dotenv
 
 
-# Create an OpenAI client with your API key
-# Load the OpenAI API key from the .env file
-load_dotenv()
+
 
 # Create an OpenAI client with your API key
 openai_client = openai.Client(api_key=st.secrets["OPENAI_API_KEY"])
@@ -48,19 +21,6 @@ assistant = openai_client.beta.assistants.retrieve(
 st.title("Deep Learning and pattern recognition Tutor")
 st.subheader("ask questions about the course")
 
-# # Create a file input for the user to upload a CSV
-# uploaded_file = st.file_uploader(
-#     "Upload a CSV", type="csv", label_visibility="collapsed"
-# )
-
-# # If the user has uploaded a file, start the assistant process...
-# if uploaded_file is not None:
-#     # Create a status indicator to show the user the assistant is working
-#     with st.status("Starting work...", expanded=False) as status_box:
-#         # Upload the file to OpenAI
-#         file = openai_client.files.create(
-#             file=uploaded_file, purpose="assistants"
-#         )
 
 
 thread = "thread_EOIKwwlYhsVb75FSA7FOKnnK"
@@ -77,9 +37,6 @@ send_button = st.button("Send")
 if send_button and question:
     with st.status("Starting work...", expanded=True) as status_box:
        
-        #     # Create a status indicator to show the user the assistant is working
-        #     with st.status("Starting work...", expanded=False) as status_box:
-        #         # Upload the file to OpenAI
 
             # Create a new thread with a message that has the uploaded file's ID
         openai_client.beta.threads.messages.create(
